46098242b2e885babd204400b249222f06c5f68f
William Horn <wbhorn@alaska.edu>
2025-04-14 10:50:57 -0800

    Remove extra function

--- src/hyp3_opera_rtc/upload_rtc.py
+++ src/hyp3_opera_rtc/upload_rtc.py
@@ -4,18 +4,12 @@ from pathlib import Path
 from hyp3lib.aws import upload_file_to_s3
 from hyp3lib.image import create_thumbnail
 
-import bucket_info_file
 
-
-def upload_rtc(bucket: str, bucket_prefix: str, work_dir: Path = Path('/home/rtc_user/scratch')) -> None:
+def upload_rtc(bucket: str, bucket_prefix: str, work_dir: Path) -> None:
     for browse in work_dir.glob('*.png'):
         create_thumbnail(browse, output_dir=work_dir)
 
-    upload_product_to_s3(work_dir, bucket, bucket_prefix)
-
-
-def upload_product_to_s3(product_dir: Path, bucket: str, bucket_prefix: str) -> None:
-    for product_file in product_dir.iterdir():
+    for product_file in work_dir.iterdir():
         if product_file.is_dir():
             continue
 
@@ -33,7 +27,7 @@ def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--bucket', required=True, help='AWS S3 bucket HyP3 uses for uploading the final products')
     parser.add_argument('--bucket-prefix', default='', help='Add a bucket prefix to products')
-    parser.add_argument('--work-dir', type=Path, default=None, help='directory with processing outputs')
+    parser.add_argument('--work-dir', type=Path, default=Path(), help='directory with processing outputs')
 
     args = parser.parse_args()
 
