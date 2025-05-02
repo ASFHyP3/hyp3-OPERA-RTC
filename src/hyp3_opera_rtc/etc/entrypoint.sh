4df66ed230f7ad9c0830051f759ad663a245d3fd
Forrest Williams <ffwilliams2@alaska.edu>
2025-05-02 12:24:33 -0500

    update dir names

--- src/hyp3_opera_rtc/etc/entrypoint.sh
+++ src/hyp3_opera_rtc/etc/entrypoint.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=$PYTHONPATH:${PGE_DEST_DIR}
 
 python -um hyp3_opera_rtc.prep_rtc "$@" --work-dir /home/rtc_user/scratch
 conda run -n RTC /home/rtc_user/opera/scripts/pge_main.py -f /home/rtc_user/scratch/runconfig.yml
-python -um hyp3_opera_rtc.upload_rtc "$@" --output-dir /home/rtc_user/scratch/output
+python -um hyp3_opera_rtc.upload_rtc "$@" --output-dir /home/rtc_user/scratch/output_dir
